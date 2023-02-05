d892cc6f3e7d6f7bcf32e3765ea0fa0da602bd74
William Aranda <wearandas.84@outlook.com>
2023-02-04 22:53:20 -0600

    Se agregan comentarios

--- Matematicas.java
+++ Matematicas.java
@@ -1,3 +1,4 @@
+//Importamos la clase Scanner para poder leer desde la linea de comandos
 import java.util.Scanner;
 
 public class Matematicas {
@@ -5,9 +6,13 @@ public class Matematicas {
         int opcion, cant;
         int ejecuta = 1;
         double num1, num2;
+        /*Inicializamos un objeto de la clase Matematicas 
+        para tener acceso a las funciones de operacion*/
         Matematicas oper = new Matematicas();
+        /* Inicializamos el Scanner para leer desde linea de comandos */
         Scanner lector = new Scanner(System.in);
         while(ejecuta == 1){
+            //Creamos un menu, el cual se ejecutara mientras hasta que el usuario desee salir
             System.out.println("Bienvenido seleccione la operacion que desea realizar:");
             System.out.println("1) Sumar");
             System.out.println("2) Restar");
@@ -15,6 +20,7 @@ public class Matematicas {
             System.out.println("4) Dividir");
             System.out.println("5) Salir");
             opcion = lector.nextInt();
+            //Para el caso de la division solo se permite para 2 numeros.
             if(opcion == 4){
                 System.out.println("Introduce el primer numero:");
                 num1 = lector.nextDouble();
@@ -23,18 +29,24 @@ public class Matematicas {
                 double resultado = oper.division(num1, num2);
                 System.out.println("El resultado de la division es: "+ resultado);
             } else {
+                //En caso de elegir salir se imprime mensaje, cerramos el lector y detenemos la ejecucion.
                 if(opcion == 5){
                     System.out.println("Gracias por utilizar la herramienta");
                     lector.close();
                     System.exit(0);
                 } else {
+                    /*Si se selecciona una operacion de Suma, Resta o Multiplicacion, se solicita la
+                     cantidad de numeros con los cuales se desea ejecutar.*/
                     System.out.println("¿Cuantos numeros desea utilizar para la operacion?:");
                     cant = lector.nextInt();
+                    /* Se inicializa un arreglo de numeros doubles para precision decimal en el cual se
+                       almacenan los numeros con un ciclo for utilizando el indice. */
                     double[] numeros = new double[cant];
                     for(int i = 0; i < cant; i++){
                         System.out.println("Introduce el numero:");
                         numeros[i] = lector.nextDouble();
                     }
+                    /* Con un switch dependiendo de la opcion seleccionada se ejecuta una funcion para la operacion. */
                     switch(opcion){
                         case 1:
                             System.out.println("El resultado de la Suma es:" + oper.sumar(numeros, cant));
@@ -56,6 +68,8 @@ public class Matematicas {
         
     }
 
+
+    //Creamos las funciones matematicas para realizar las operaciones.
     public double sumar(double[] numeros, int cant){
         double resultado=0;
         for(int i = 0; i < cant; i++){
@@ -68,6 +82,7 @@ public class Matematicas {
         double resultado=0;
         for(int i = 0; i < cant; i++){
             if(i == 0){
+                //Si es el primer numero se almacena en la variable resultado y a partir del segundo se resta para evitar errores.
                 resultado = numeros[i];
             } else {
                 resultado -= numeros[i];
@@ -81,6 +96,7 @@ public class Matematicas {
         double resultado=0;
         for(int i = 0; i < cant; i++){
             if(i == 0){
+                //Si es el primer numero se almacena en la variable resultado y a partir del segundo se multiplica para evitar errores.
                 resultado = numeros[i];
             } else {
                 resultado *= numeros[i];
